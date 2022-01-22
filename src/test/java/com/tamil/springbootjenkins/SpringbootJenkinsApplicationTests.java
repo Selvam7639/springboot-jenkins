1e5702c369b44fd605c14cbdfa6ed387408a71c4
Tamizhselvam Selvam <selvamtamil47@gmail.com>
2022-01-22 12:44:17 +0530

    Fisrt Commit

---
+++ src/test/java/com/tamil/springbootjenkins/SpringbootJenkinsApplicationTests.java
@@ -0,0 +1,20 @@
+package com.tamil.springbootjenkins;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootJenkinsApplicationTests {
+
+	public static Logger logger = LoggerFactory.getLogger(SpringbootJenkinsApplicationTests.class);
+
+	@Test
+	void contextLoads() {
+		logger.info("Test case executing...");
+		Assertions.assertTrue(true);
+	}
+
+}
