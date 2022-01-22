724e2c0dcbe8957aaad531c178e0d627f7b712ba
Tamizhselvam Selvam <selvamtamil47@gmail.com>
2022-01-22 15:30:32 +0530

    second commit

--- src/test/java/com/tamil/springbootjenkins/SpringbootJenkinsApplicationTests.java
+++ src/test/java/com/tamil/springbootjenkins/SpringbootJenkinsApplicationTests.java
@@ -14,6 +14,7 @@ class SpringbootJenkinsApplicationTests {
 	@Test
 	void contextLoads() {
 		logger.info("Test case executing...");
+		logger.info("Test case executing2...");
 		Assertions.assertTrue(true);
 	}
 
